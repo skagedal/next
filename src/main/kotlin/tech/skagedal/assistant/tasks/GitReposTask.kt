dced5f4c99cf148b20c39d33bf32708d69e71ab1
Simon Kågedal Reimer <skagedal@gmail.com>
2020-10-24 20:45:06 +0200

    Add check for non-merged branches in git task

--- src/main/kotlin/tech/skagedal/assistant/tasks/GitReposTask.kt
+++ src/main/kotlin/tech/skagedal/assistant/tasks/GitReposTask.kt
@@ -6,13 +6,18 @@ import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.runBlocking
 import tech.skagedal.assistant.RunnableTask
 import tech.skagedal.assistant.TaskResult
+import tech.skagedal.assistant.git.GitRepo
 import tech.skagedal.assistant.isGloballyIgnored
 import java.nio.file.Files
 import java.nio.file.Path
 
 class GitReposTask(val path: Path) : RunnableTask {
-    enum class GitResult {
-        CLEAN, DIRTY, NOT_A_GIT_REPOSITORY, NOT_A_DIRECTORY
+    sealed class GitResult {
+        object Clean: GitResult()
+        object Dirty: GitResult()
+        object NotGitRepository: GitResult()
+        object NotDirectory: GitResult()
+        data class UnmergedBranches(val branches: List<String>): GitResult()
     }
 
     data class ResultWithPath(
@@ -21,21 +26,28 @@ class GitReposTask(val path: Path) : RunnableTask {
     )
 
     override fun runTask() = fetchAllResults()
-        .find { it.result != GitResult.CLEAN }?.let {
-            when (it.result) {
-                GitResult.DIRTY -> {
+        .find { it.result != GitResult.Clean }?.let {
+            val result = it.result
+            when (result) {
+                GitResult.Dirty -> {
                     System.err.println("Dirty git repository")
                     TaskResult.ShellActionRequired(it.path)
                 }
-                GitResult.NOT_A_GIT_REPOSITORY -> {
+                GitResult.NotGitRepository -> {
                     System.err.println("Not a git repository")
                     TaskResult.ShellActionRequired(it.path)
                 }
-                GitResult.NOT_A_DIRECTORY -> {
+                GitResult.NotDirectory -> {
                     System.err.println("Not a directory: ${it.path}")
                     TaskResult.ShellActionRequired(path)
                 }
-                GitResult.CLEAN -> throw IllegalStateException()
+                GitResult.Clean -> throw IllegalStateException()
+                is GitResult.UnmergedBranches -> {
+                    System.err.println("Contains unmerged branches: ${it.path}")
+                    result.branches.forEach(::println)
+                    System.err.println("Run simons-assistant git-clean")
+                    TaskResult.ShellActionRequired(path)
+                }
             }
         } ?: TaskResult.Proceed
 
@@ -50,14 +62,18 @@ class GitReposTask(val path: Path) : RunnableTask {
 
     private fun repoResult(dir: Path): GitResult {
         if (!Files.isDirectory(dir)) {
-            return if (dir.isGloballyIgnored()) GitResult.CLEAN else GitResult.NOT_A_DIRECTORY
+            return if (dir.isGloballyIgnored()) GitResult.Clean else GitResult.NotDirectory
         }
         val statusResult = gitStatusResult(dir)
-        if (statusResult != GitResult.CLEAN) {
+        if (statusResult != GitResult.Clean) {
             return statusResult
         }
 
-        return GitResult.CLEAN
+        val notMergedBranches = GitRepo(dir).getNotMergedBranches()
+        if (notMergedBranches.isNotEmpty()) {
+            return GitResult.UnmergedBranches(notMergedBranches)
+        }
+        return GitResult.Clean
     }
 
     private fun gitStatusResult(dir: Path): GitResult {
@@ -66,13 +82,13 @@ class GitReposTask(val path: Path) : RunnableTask {
             .start()
         val exitCode = process.waitFor()
         if (exitCode != 0) {
-            return GitResult.NOT_A_GIT_REPOSITORY
+            return GitResult.NotGitRepository
         }
         val nonEmptyGitStatusResult = process
             .inputStream
             .readAllBytes()
             .isNotEmpty()
-        return if (nonEmptyGitStatusResult) GitResult.DIRTY else GitResult.CLEAN
+        return if (nonEmptyGitStatusResult) GitResult.Dirty else GitResult.Clean
     }
 }
 
