c7dab66f267aef329cef573289739bf5a136122f
Simon Kågedal Reimer <skagedal@gmail.com>
2021-01-11 09:20:15 +0100

    Add default-branches command line argument

--- src/main/kotlin/tech/skagedal/assistant/commands/GitReposCommand.kt
+++ src/main/kotlin/tech/skagedal/assistant/commands/GitReposCommand.kt
@@ -1,6 +1,8 @@
 package tech.skagedal.assistant.commands
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.split
 import tech.skagedal.assistant.services.GitReposService
 import java.nio.file.FileSystem
 
@@ -8,7 +10,16 @@ class GitReposCommand(
     private val fileSystem: FileSystem,
     private val gitReposService: GitReposService
 ) : CliktCommand(name = "git-repos") {
+    val defaultBranches by option(
+        "-b",
+        metavar = "DEFAULTBRANCHES",
+        help = "Comma-separated list of branches that should be checked out when we leave the task.  For each repo, these branches are tried in order."
+    ).split(",")
+
     override fun run() {
+        defaultBranches?.apply {
+            forEach(::println)
+        }
         val path = fileSystem.getPath(".")
         gitReposService.handleAllGitRepos(path)
     }
