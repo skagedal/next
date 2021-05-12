f56671bacc6409fd05582b1ca336ed9962e9fbe3
Simon Kågedal Reimer <skagedal@gmail.com>
2021-05-12 09:14:26 +0200

    Remove n shell function, replaced with rust assistant

--- shell/simons-assistant.zsh
+++ shell/simons-assistant.zsh
@@ -1,11 +1,3 @@
-function n() {
-  simons-assistant next
-  return_value=$?
-  if [ $return_value -eq 10 ]; then
-    cd "$(<~/.simons-assistant/data/requested-directory)"
-  fi
-}
-
 function work() {
   simons-assistant track-start
 }
